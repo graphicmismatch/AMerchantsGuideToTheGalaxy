8352569687653cabbd5cd81510b510157549f845
RayanMadan <riptide.syncopate@gmail.com>
2022-08-28 03:51:29 +0530

    added day timer and fraamework for all scenes

--- Assets/Scripts/Data.cs
+++ Assets/Scripts/Data.cs
@@ -6,12 +6,35 @@ public class Data : MonoBehaviour
 {
     static int seed;
     static int currplanet = -1;
+    static int days = 0;
+    float duration = 20f;
+    bool dotime;
     private void Awake()
     {
         DontDestroyOnLoad(this);
+        dotime = false;
     }
     private void Update()
     {
+        if (SceneManager.GetActiveScene().name == "SpaceScene")
+        {
+            dotime = true;
+        }
+        if (SceneManager.GetActiveScene().name == "WinScene"|| SceneManager.GetActiveScene().name == "LoseScene")
+        {
+            dotime = false;
+        }
+
+        if (dotime)
+        {
+            duration -= Time.deltaTime;
+            if (duration <= 0f)
+            {
+                days++;
+                duration = 20f;
+            }
+        }
+
     }
     public static void setSeed(int s)
     {
